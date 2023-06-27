1dee677939f99b32f9913527fea8668b054ee265
edineipinho <edneyrock@gmail.com>
2023-06-26 23:37:27 -0300

    :sparkles: feat: api get_all  works

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -1,3 +1,19 @@
 const CarroService = require('../services/CarroService')
 
 // Tudo que for regra do banco de dados vai ser usado aqui.
+
+
+module.exports = {
+  buscarTodos: async (req, res) => {
+    let json = { error: '', result: [] }
+    let carros = await CarroService.buscarTodos()
+
+    for (let i in carros) {
+      json.result.push({
+        codigo: carros[i].codigo,
+        descricao: carros[i].modelo,
+      })
+    }
+    res.json(json)
+  }
+}
